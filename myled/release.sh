8a0e6c9cf43a72ea8b5c093a3e89083b2ba21d1a
Chappy06260 <50652151+Chappy06260@users.noreply.github.com>
2021-01-07 16:35:22 +0900

    Update release.sh

--- myled/release.sh
+++ myled/release.sh
@@ -5,7 +5,7 @@ sudo rmmod myled
 make clean
 
 chmod -x myled.c
-chmod -x ~/catkin_ws/src/robosys_ros/scripts/led_sub.py
-chmod -x ~/catkin_ws/src/robosys_ros/scripts/led_pub.py
+chmod -x ~/catkin_ws/src/Robosys2020_ROS_LED/scripts/led_sub.py
+chmod -x ~/catkin_ws/src/Robosys2020_ROS_LED/scripts/led_pub.py
 
 
